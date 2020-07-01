57a36bc26c597e84ef53c3ac627911e17955357f
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-07-01 15:18:13 +0800

    initalize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+
+
+export default function App() {
+  return <h1>app component</h1>;
+}
